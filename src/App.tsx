807e859ac70ce311d4103f72ae2bef0394e1f05e
veryt <verytallman993@gmail.com>
2023-04-18 10:59:38 +0400

    add start components for lessons

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>This is App component</div>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 }
 
 export default App;
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>
+            <div>star</div>
+        </div>
+    );
+}
+
+
+function Accordion() {
+    return (
+        <div>
+          <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    );
+}
+
+function AccordionTitle() {
+    return (
+        <div>
+            <div>Menu</div>
+        </div>
+    );
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <div>Menu</div>
+        </div>
+    );
+}
+
